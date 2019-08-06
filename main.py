36d4cf66b4b69f7bf1c0a51a2c5757e80e743326
Ubuntu <ubuntu@ip-172-31-82-209.ec2.internal>
2019-08-06 21:16:33 +0000

    adding

---
+++ main.py
@@ -0,0 +1,8 @@
+import webapp2
+import jinja2
+import os
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
