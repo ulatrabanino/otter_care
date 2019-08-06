f2f9e1d61cc18520e2351918cd94d9714d2e222b
Ubuntu <ubuntu@ip-172-31-82-209.ec2.internal>
2019-08-06 22:03:30 +0000

    adding

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+    
+class MainPage(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        welcome_template = the_jinja_env.get_template('templates/home.html')
+        self.response.write(welcome_template.render())
+        
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
