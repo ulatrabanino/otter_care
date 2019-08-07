e677c798cd3ccddd6cefef22087befbe26363d8f
Ubuntu <ubuntu@ip-172-31-92-115.ec2.internal>
2019-08-07 16:36:40 +0000

    for data base properties

--- users.py
+++ users.py
@@ -3,10 +3,10 @@ from google.appengine.ext import ndb
 class Otter(ndb.Model):
     otter_name = ndb.StringProperty(required=True)
     email_address = ndb.StringProperty(required=True)
-    water_intake = ndb.StringProperty(required=False)
-    food_intake = ndb.StringProperty(required=False)
-    exercise = ndb.StringProperty(required=False)
-    bath = ndb.StringProperty(required=False)
+    water_intake = ndb.DateTimeProperty(required=False)
+    food_intake = ndb.DateTimeProperty(required=False)
+    exercise = ndb.DateTimeProperty(required=False)
+    bath = ndb.DateTimeProperty(required=False)
 
 class OtterUser(ndb.Model):
     first_name = ndb.StringProperty()
